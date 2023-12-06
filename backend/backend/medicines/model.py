01eb86cfe2f99e93490c0f44573dac8fd7acd63c
florenceabbo <fulumera@gmail.com>
2023-12-05 22:33:32 -0800

    edited files

--- backend/backend/medicines/model.py
+++ backend/backend/medicines/model.py
@@ -13,6 +13,8 @@ class Medicine(db.Model):
   created_by :str
   created_at :str
   updated_at :str
+  expiry_date: str
+
   id = db.Column(db.Integer, primary_key = True)
   name = db.Column(db.String(100),unique=True)
   unit_price = db.Column(db.String(10))
@@ -22,32 +24,28 @@ class Medicine(db.Model):
   created_at = db.Column(db.String(255),nullable=True)
   updated_at = db.Column(db.String(255),nullable=True)
   medicine_category_id= db.Column(db.Integer, db.ForeignKey('medicine_categories.id'))
+  expiry_date = db.Column(db.String(255), nullable=True)  # New column for expiry date
   
 
 
-  def __init__(self, name,image,unit_price,stock,created_by,created_at,updated_at, medicine_category_id):
-   self.name = name
-   self.image = image
-   self.unit_price = unit_price 
-   self.stock = stock
-   self.created_by = created_by
-   self.created_at = created_at
-   self.updated_at =updated_at
-   self.medicine_category_id= medicine_category_id
-
-  
+def __init__(self, name, image, unit_price, stock, created_by, created_at, updated_at, medicine_category_id, expiry_date):
+      self.name = name
+      self.image = image
+      self.unit_price = unit_price
+      self.stock = stock
+      self.created_by = created_by
+      self.created_at = created_at
+      self.updated_at = updated_at
+      self.medicine_category_id = medicine_category_id
+      self.expiry_date = expiry_date  # Initialize the new column
 
 def __repr__(self):
-  return f"<Medicine {self.name} >"
-  
+      return f"<Medicine {self.name}>"
 
-        
-   #save a new instance
 def save(self):
-    db.session.add(self)
-    db.session.commit()
+      db.session.add(self)
+      db.session.commit()
 
-   #delete the item
 def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+     db.session.delete(self)
+     db.session.commit()
